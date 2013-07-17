ac229bc0811555791ac559aee2bb4429b6e28c9a
Ryan March <rmarch03@villanova.edu>
2013-07-17 15:26:52 -0400

    First Commit

---
+++ vendor/bundle/gems/atomic-1.1.10/ext/siteconf20130717-649-q0snik.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130717-649-18y271j"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
